f82b6d854aa99dbc33337136de182b9267b8e82e
Nintenmike3dsx <mjarend04@gmail.com>
2026-04-07 13:32:19 -0400

    added initial recipe card

--- recipes.js
+++ recipes.js
@@ -5,16 +5,20 @@ async function loadRecipes() {
   grid.innerHTML = '';
 
   recipes.forEach(recipe => {
-    grid.innerHTML += `
-      <div class="col-6 col-md-3 mb-4">
-        <div class="card h-100">
-          <img src="dataset/images/${recipe.images[0]}" class="card-img-top" style="height:180px;object-fit:cover;">
-          <div class="card-body">
-            <h6 class="card-title">${recipe.name}</h6>
-          </div>
+    const col = document.createElement('div');
+    col.className = 'col-6 col-md-3 mb-4';
+    col.innerHTML = `
+      <div class="card h-100" style="cursor:pointer;">
+        <img src="dataset/images/${recipe.images[0]}" class="card-img-top" style="height:180px;object-fit:cover;">
+        <div class="card-body">
+          <h6 class="card-title">${recipe.name}</h6>
         </div>
       </div>`;
+    col.addEventListener('click', () => {
+      localStorage.setItem('selectedRecipe', JSON.stringify(recipe));
+      window.location.href = 'recipe.html';
+    });
+    grid.appendChild(col);
   });
 }
-
 document.addEventListener('DOMContentLoaded', loadRecipes);
