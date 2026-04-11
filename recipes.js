096aa4a57cd109d47cdb4cda0d9ce05c0ceff239
Nintenmike3dsx <mjarend04@gmail.com>
2026-04-11 16:30:15 -0400

    converted list to have More button as requested

--- recipes.js
+++ recipes.js
@@ -1,9 +1,17 @@
 let allRecipes = [];
+let limiter = 4; //we can change this to whatever but the page shows 4 at a time rn
 
-function renderRecipes(recipes) {
+function showRecipes(recipes) {
+  filteredRecipes = recipes;
+  limiter = 4;
+  displayRecipes();
+}
+
+function displayRecipes() {
   const grid = document.getElementById('recipe-grid');
   grid.innerHTML = '';
-  recipes.forEach(recipe => {
+
+  filteredRecipes.slice(0, limiter).forEach(recipe => {
     const col = document.createElement('div');
     col.className = 'col-6 col-md-3 mb-4';
     col.innerHTML = `
@@ -19,16 +27,27 @@ function renderRecipes(recipes) {
     });
     grid.appendChild(col);
   });
+
+  if (limiter < filteredRecipes.length) {
+    const btnCol = document.createElement('div');
+    btnCol.className = 'col-12 text-center mt-2 mb-4';
+    btnCol.innerHTML = `<button class="btn btn-dark" id="more">More</button>`;
+    grid.appendChild(btnCol);
+    document.getElementById('more').addEventListener('click', () => {
+      limiter += 4; //this can also be adjusted but 4 is what the page shows
+      displayRecipes();
+    });
+  }
 }
 
 async function loadRecipes() {
   const res = await fetch('dataset/recipes/all_recipes.json');
   allRecipes = await res.json();
-  renderRecipes(allRecipes);
+  showRecipes(allRecipes);
 
   document.getElementById('search-input').addEventListener('input', function() {
     const query = this.value.toLowerCase();
-    renderRecipes(allRecipes.filter(r => r.name.toLowerCase().includes(query)));
+    showRecipes(allRecipes.filter(r => r.name.toLowerCase().includes(query)));
   });
 }
 
